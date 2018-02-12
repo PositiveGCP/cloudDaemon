27f3baa43f5d339112f52d48f6ad53b0be9703fb
danteese <dalnte@me.com>
2018-02-11 22:08:21 -0600

    Cambio de URL de servicio

--- keys.py
+++ keys.py
@@ -11,5 +11,7 @@ config = {
 email = "dante.bazaldua@positivecompliance.com"
 passwd = "3Xt1ns10n"
 N_MS_AUTHCB = 'C8ADA7F4-4627-4461-99C9-008FC68B1209'
-urlToQuery = "http://service.lva-i.com/LVAnalysis/?actionType=2&audioStreamUrl="
-RA7url = "http://ra7.nemesysco.net/RAnalysis/?actionType=2&audioStreamURL="
+urlToQuery = (
+    "http://service.lva-i.com/LVAnalysis/?actionType=2&audioStreamUrl="
+)
+ra7 = "http://rservice.nemesysco.net/RAnalysis/?actionType=2&audioStreamUrl="
