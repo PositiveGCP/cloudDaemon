934a1f4c94015b1286b795456c78e2c4bcd1e325
danteese <dalnte@me.com>
2018-02-11 22:07:19 -0600

    Soporte RA7

--- mainCLVA.py
+++ mainCLVA.py
@@ -1,22 +1,26 @@
-#*- coding: utf-8 -*
-# @author: Dante Fernando Bazaldua Huerta
-# Segunda parte del cloud servicie que permite conexión con el servidor en Israel
-
+# *- coding: utf-8 -*
+# @author: Dante Fernando Bazaldua Huerta
+# Manage all connections
 import pyrebase
-import requests # Hacer query a CLVA-i
+import requests  # Hacer query a CLVA-i
 import time
 import keys as security
+import urllib
 
-firebase = pyrebase.initialize_app( security.config ) # Inicializar firebase
+# Inicializar firebase
+firebase = pyrebase.initialize_app(security.config)
 
 # ------------------------ FIREBASE -------------------------
-auth = firebase.auth() # Objecto de autenticación
-user = auth.sign_in_with_email_and_password( security.email, security.passwd ) # Iniciar sesion en firebase
-storage = firebase.storage()    # Referencia al storage
-db = firebase.database()        # Referencia a la base de datos
+auth = firebase.auth()  # Objecto de autenticación
+# Iniciar sesion en firebase
+user = auth.sign_in_with_email_and_password(security.email, security.passwd)
+storage = firebase.storage()  # Referencia al storage
+db = firebase.database()  # Referencia a la base de datos
 
-# Clase para cada uno de los n objetos que pudieran suceder al leer el archivo de audio
-class LvaProcess( object ):
+
+# Clase para cada uno de los n objetos que pudieran
+# suceder al leer el archivo de audio
+class LvaProcess(object):
 
     link = ""
     response = ""
@@ -25,7 +29,7 @@ class LvaProcess( object ):
     uid = ""
     URL_inside = ""
 
-    def __init__( self, link, uid ):
+    def __init__(self, link, uid):
         self.link = link
         self.uid = uid
         self.response = ""
@@ -34,7 +38,7 @@ class LvaProcess( object ):
         # self._processFile()
 
     # Codifica la URL
-    def encodeURL(self, urlin ):
+    def encodeURL(self, urlin):
         txt = ""
         tmp = ''
         for c in urlin:
@@ -45,7 +49,7 @@ class LvaProcess( object ):
             elif c is '&':
                 tmp = '%26'
             elif c is '/':
-        	    tmp = '%2F'
+                tmp = '%2F'
             elif c is ':':
                 tmp = '%3A'
             elif c is '%':
@@ -57,76 +61,88 @@ class LvaProcess( object ):
         # End for loop
         return txt
 
-    # Proceso que ejecuta conexión con el storage
-    def _processFile( self ):
+    # Proceso que ejecuta conexión con el storage
+    def _processFile(self):
         URL = '/cloud/' + self.uid + '/' + self.link
         try:
             # Ejecutar solicitud
-            audio   = storage.child( URL ).get_url(user['idToken'])
+            audio = storage.child(URL).get_url(user['idToken'])
             # print user['idToken']
             # print "URL de descarga: -> %s" %(audio)
 
-        	# Codificar la URL
+            # Codificar la URL
             e = audio
-            headers = {'Accept': 'application/json',
-        		   'Content-Type':'application/json',
-        		   'Cache-Control':'no-cache',
-        		   'N-MS-AUTHCB': security.N_MS_AUTHCB }
-            url_encoded = self.encodeURL( e )
+            headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Cache-Control': 'no-cache',
+                'N-MS-AUTHCB': security.N_MS_AUTHCB
+                }
+            service = security.ra7
+            url_encoded = urllib.quote(e, safe='')
 
             # print "URL_encoded de descarga: -> %s" %(url_encoded)
             # Truncate the string
             # info = (e[75:150] + '...') if len(e) > 200 else e
             # print info
-            line_req = security.urlToQuery + url_encoded
-            # Ejecuta la consulta
-            req = requests.get( line_req, headers=headers )
+            line_req = service + url_encoded
+            # Ejecuta la consulta
+            req = requests.get(line_req, headers=headers)
             self.response = req.json()
             self.status = req.status_code
             self.response_text = req.text
-            rsp = "\t\t" + self.uid + " -> " + self.link + " [ "+ self.response_text +" ]"
+            rsp = (
+                "\t\t"
+                + self.uid +
+                " -> "
+                + self.link +
+                " [ "
+                + self.response_text +
+                " ]"
+            )
             return rsp
             # print "\t\t %s -> %s [OK]" %(str(self.uid), str(self.link))
             # TODO: Guardar la respuesta en el archivo de texto
 
         except Exception as e:
-            # Cachar excepciones
-            print "Ocurrió excepcion en LvaProcess %s" %( str(e) )
+            print "Problema en LvaProcess %s" % (str(e))
 
-class Transaction( object ):
+
+class Transaction(object):
     uid = ""
 
     # Constructor:
     # Id de la transaccion
-    def __init__( self, uid ):
+    def __init__(self, uid):
         self.uid = uid
         pass
 
     # Actualiza la rama con el resultado y la fecha
-    def updateBranch( self, information ):
-    	try:
-    	    timex = time.localtime(time.time()) # Obtener el tiempo actual en el que se realiza la transaccion
-    	    transaction = db.child("Transfer/" + self.uid ).get()
-            # TODO: en la versión de produccion quitar la siguiente linea
-            # information = "TEST-10|20|30|40"
-    	    th = transaction.val()
-    		# Cambiar los datos en la rama
-    	    th["resultado"] = information
-    	    th["date_final"] = time.strftime( '%Y/%m/%d - %H:%M:%S', timex )
+    def updateBranch(self, information):
+        try:
+            # Obtener el tiempo actual en el que se realiza la transaccion
+            timex = time.localtime(time.time())
+            transaction = db.child("Transfer/" + self.uid).get()
+            th = transaction.val()
+            # Cambiar los datos en la rama
+            th["resultado"] = information
+            th["date_final"] = time.strftime('%Y/%m/%d - %H:%M:%S %Z', timex)
             th["processed"] = True
-    		# print users
-    	    db.child("Transfer").child( self.uid ).set( th )
+            # print users
+            db.child("Transfer").child(self.uid).set(th)
 
-    	except Exception as e:
-            # TODO: mejorar el caching pues pudieron pasar muchas cosas como:
+        except Exception as e:
+            # TODO: mejorar el caching pues pudieron pasar muchas cosas como:
             # - No se encontró la rama ( fue eliminada por alguna razón )
-            # - No se pudo obtener de manera correcta el resultado o fecha
-    		print e
+            # - No se pudo obtener de manera correcta el resultado o fecha
+            print e
 
 
-
-# Clase que obtiene todo el archivo de audio y garantiza un UID para la transaccion
-class AudioFile( object ):
+class AudioFile(object):
+    """
+    Clase que obtiene todo el archivo de audio
+    y garantiza un UID para la transacción
+    """
 
     audioFile = None
     # Rutas relativas para cada audio:
@@ -134,31 +150,36 @@ class AudioFile( object ):
     uid = ""
 
     # Constructor, asigna un uid para concretar la transaccion
-    def __init__( self, uid ):
+    def __init__(self, uid):
         self.paths = []
         self.uid = uid
-        self.getProcessingFile() # Obtener el archivo de procesamiento automaticamente
+        # Obtener el archivo de procesamiento automaticamente
+        self.getProcessingFile()
 
     # Obtener el archivo con los audios
-    def getProcessingFile( self ):
+    def getProcessingFile(self):
         try:
-            self.audioFile   = storage.child('/cloud/'+self.uid+'/processing.txt').get_url(user['idToken'])
+            poi = '/cloud/'+self.uid+'/processing.txt'
+            self.audioFile = storage.child(poi).get_url(user['idToken'])
             # print "URL de descarga: -> %s" %(self.audioFile)
             if self.audioFile is not '':
-                # Aquí se recibe el link del archivo que contiene ubicación de los audios
-                self.process_file( self.audioFile ) # Procesamiento del URL para obtener los audios
+                # Aquí se recibe el link del archivo que contiene
+                # ubicación de los audios
+                # Procesamiento del URL para obtener los audios
+                self.process_file(self.audioFile)
 
         except Exception as e:
             log = "URL ERROR - " + str(e)
             print log
 
     # Modulo de proceso del archivo de texto
-    def process_file( self, URL ):
+    def process_file(self, URL):
         try:
             # Ejecutar solicitud
-            req = requests.get( URL )
-            self.readlineByLine( req.text )
-            # TODO: Mejorar el registro de actividades pues pudieron haber sucedido las siguientes:
+            req = requests.get(URL)
+            self.readlineByLine(req.text)
+            # TODO: Mejorar el registro de actividades pues pudieron
+            # haber sucedido las siguientes:
             # - El archivo no está bien escrito
             # - El achivo no contenía los correctos archivos
 
@@ -166,19 +187,19 @@ class AudioFile( object ):
             print str(e)
 
     # Lee linea por linea el texto completo
-    def readlineByLine( self, raw ):
+    def readlineByLine(self, raw):
         i = 0
         sep = raw.split('\n')
-        tot = len( sep )
+        tot = len(sep)
         try:
             for line in sep:
                 if i > 4:
-                    realAudio = self.parseLine( line )
+                    realAudio = self.parseLine(line)
                     new = realAudio.split('\r')
-    	            # print "L[%d] - %s" %( i, new )
-                    self.paths.append( str(new[0]) )
+                    # print "L[%d] - %s" %( i, new )
+                    self.paths.append(str(new[0]))
                     if i == (tot - 2):
-                        break;
+                        break
                 i = i + 1
 
         except Exception as e:
@@ -186,8 +207,8 @@ class AudioFile( object ):
             # No Exception
 
     # Separa la linea y devuelve donde se encuentra cada archivo
-    def parseLine( self, lineCode ):
+    def parseLine(self, lineCode):
         # print lineCode
-        separated = lineCode.split( '||' )
+        separated = lineCode.split('||')
         last = separated[4].split('\\')
         return last[4]
