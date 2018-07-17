79b6b56cfe4f1b63d1e900d1e11c4d709405cc37
Danteese <dalnte@me.com>
2018-07-17 15:14:12 +0200

    Firefighter

--- keys.py
+++ keys.py
@@ -1,13 +1,23 @@
 # *- coding: utf-8 -*
 
+# config = {
+#     "apiKey": "AIzaSyAT7spVMFGob7q6Q1UJCaMi6RvGoMBgcAc",
+#     "authDomain": "prototipo1-8e37a.firebaseapp.com",
+#     "databaseURL": "https://prototipo1-8e37a.firebaseio.com",
+#     "projectId": "prototipo1-8e37a",
+#     "storageBucket": "prototipo1-8e37a.appspot.com",
+#     "messagingSenderId": "856846236373"
+# }
+
 config = {
-    "apiKey": "AIzaSyAT7spVMFGob7q6Q1UJCaMi6RvGoMBgcAc",
-    "authDomain": "prototipo1-8e37a.firebaseapp.com",
-    "databaseURL": "https://prototipo1-8e37a.firebaseio.com",
-    "projectId": "prototipo1-8e37a",
-    "storageBucket": "prototipo1-8e37a.appspot.com",
-    "messagingSenderId": "856846236373"
+    "apiKey": "AIzaSyAhDYV5B4WnGz1S-ewBWNiow-cWB85T3-I",
+    "authDomain": "prototipo2-5af1f.firebaseapp.com",
+    "databaseURL": "https://prototipo2-5af1f.firebaseio.com",
+    "projectId": "prototipo2-5af1f",
+    "storageBucket": "prototipo2-5af1f.appspot.com",
+    "messagingSenderId": "428785972383"
 }
+
 email = "dante.bazaldua@positivecompliance.com"
 passwd = "3Xt1ns10n"
 N_MS_AUTHCB = 'C8ADA7F4-4627-4461-99C9-008FC68B1209'
--- node/listen.js
+++ node/listen.js
@@ -8,12 +8,22 @@ const exec = require('child_process').exec;
 var moment = require('moment');
 
 // Initialize Firebase
+// var appConfig = {
+//   apiKey: "AIzaSyAT7spVMFGob7q6Q1UJCaMi6RvGoMBgcAc",
+//   authDomain: "prototipo1-8e37a.firebaseapp.com",
+//   databaseURL: "https://prototipo1-8e37a.firebaseio.com",
+//   storageBucket: "prototipo1-8e37a.appspot.com",
+//   messagingSenderId: "856846236373"
+// };
+
+// firefighter
 var appConfig = {
-  apiKey: "AIzaSyAT7spVMFGob7q6Q1UJCaMi6RvGoMBgcAc",
-  authDomain: "prototipo1-8e37a.firebaseapp.com",
-  databaseURL: "https://prototipo1-8e37a.firebaseio.com",
-  storageBucket: "prototipo1-8e37a.appspot.com",
-  messagingSenderId: "856846236373"
+  apiKey: "AIzaSyAhDYV5B4WnGz1S-ewBWNiow-cWB85T3-I",
+  authDomain: "prototipo2-5af1f.firebaseapp.com",
+  databaseURL: "https://prototipo2-5af1f.firebaseio.com",
+  projectId: "prototipo2-5af1f",
+  storageBucket: "prototipo2-5af1f.appspot.com",
+  messagingSenderId: "428785972383"
 };
 
 // var appConfig = {
